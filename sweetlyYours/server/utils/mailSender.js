e1aa75ff1ffad2bda1b3d74ff5b630464b3ba382
Anju <142292490+logicscienc@users.noreply.github.com>
2026-08-01 20:13:23 +0530

    Refactor mailSender function for cleaner code

--- sweetlyYours/server/utils/mailSender.js
+++ sweetlyYours/server/utils/mailSender.js
@@ -1,44 +1,28 @@
 const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
-
-  console.log("MAILSENDER START");
-console.log("MAIL_HOST:", process.env.MAIL_HOST);
-console.log("MAIL_USER:", process.env.MAIL_USER);
-
-     if (!email || !title || !body) {
-    throw new Error("Missing email, title or body");
-  }
   try {
-    console.log("CREATING TRANSPORTER");
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: 465,
-      secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
-      },
+      }
     });
-
-    await transporter.verify();
-console.log("SMTP VERIFIED");
-
-    console.log("TRANSPORTER CREATED");
 
     let info = await transporter.sendMail({
-      from: `"🍫 CaramelCorner - Because You Deserve a Treat!" <${process.env.MAIL_USER}>`  ,
-      to: `${email}`,
-      subject: `${title}`,
-      html: `${body}`,
+      from: `"CaramelCorner" <${process.env.MAIL_USER}>`,
+      to: email,
+      subject: title,
+      html: body,
     });
 
-    console.log("EMAIL SENT");
+    console.log("Mail sent:", info.messageId);
 
-    console.log("Mail sent successfully:", info.messageId);
     return info;
-  } catch (error) {
-    console.log("Error sending email:", error.message);
+
+  } catch(error) {
+    console.log("Mail error:", error.message);
     throw error;
   }
 };
